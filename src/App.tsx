fca486cfe9576c643732a3684a6006c6d872c88d
sheykisses <dancilhershey65@gmail.com>
2025-02-12 21:16:57 +0800

    Edited import Menu from './pages/About' to './pages/Menu'

--- src/App.tsx
+++ src/App.tsx
@@ -32,9 +32,8 @@ import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import Home from './pages/Home';
 import Login from './pages/Login';
-import Menu from './pages/About';
+import Menu from './pages/Menu';
 
 setupIonicReact();
 
