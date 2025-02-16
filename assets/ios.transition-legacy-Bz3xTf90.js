3c1b7bfdf75e0ac302d798436ed4005d1363deb2
Junessa <bahianjunessa@gmail.com>
2025-02-16 12:11:22 +0800

    Updates

--- assets/ios.transition-legacy-Bz3xTf90.js
+++ assets/ios.transition-legacy-Bz3xTf90.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BU-TDvUB.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-Duje9__X.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
