98722bbbf42c3ed23465dd9b9631318d752d50d6
Junessa <bahianjunessa@gmail.com>
2025-02-16 11:12:33 +0800

    Added Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,30 @@
+
 import { 
+  IonButton,
   IonButtons,
-    IonContent, 
-    IonHeader, 
-    IonMenuButton, 
-    IonPage, 
-    IonTitle, 
-    IonToolbar 
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar 
 } from '@ionic/react';
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot='start'>
-            <IonMenuButton></IonMenuButton>
+return (
+  <IonPage>
+    <IonHeader>
+    <IonToolbar>
+    <IonButtons slot='start'>
+            <IonMenuButton ></IonMenuButton>
           </IonButtons>
           <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+      <IonButton routerLink="/it35b-lab/app/home/details" expand="full">
+            Details
+        </IonButton>
       </IonContent>
     </IonPage>
   );
