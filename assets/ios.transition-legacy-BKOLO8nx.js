209331c157a0c9134c9cc90c485259d090e958fa
Junessa19 <bahianjunessa@gmail.com>
2025-03-08 14:31:32 +0800

    Updates

--- assets/ios.transition-legacy-BKOLO8nx.js
+++ assets/ios.transition-legacy-BKOLO8nx.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CgDFzh2a.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-1NQzGdKn.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
