270ef3a1af7836eca82d3a2cc527866b3e59c11e
Junessa19 <bahianjunessa@gmail.com>
2025-03-10 14:22:00 +0800

    Added updated Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,13 +2,24 @@ import {
     IonButtons,
       IonContent, 
       IonHeader, 
+      IonInput, 
+      IonItem, 
+      IonList, 
       IonMenuButton, 
       IonPage, 
       IonTitle, 
-      IonToolbar 
+      IonToolbar, 
+      useIonRouter
   } from '@ionic/react';
   
-  const Register: React.FC = () => {
+  
+ const Register: React.FC = () => {
+   const navigation = useIonRouter();
+   const doSignup = () => {
+    navigation.push('/Register', 'forward', 'replace'); 
+  };
+
+  
     return (
       <IonPage>
         <IonHeader>
@@ -20,9 +31,30 @@ import {
           </IonToolbar>
         </IonHeader>  
         <IonContent fullscreen>
+        <IonList>
+      <IonItem>
+        <IonInput label="User Name" placeholder="Name"></IonInput>
+      </IonItem>
+
+      <IonItem>
+        <IonInput label="Email input" type="email" placeholder="email@domain.com"></IonInput>
+      </IonItem>
+
+      <IonItem>
+        <IonInput label="Password input" type="password" value="password"></IonInput>
+      </IonItem>
+
+      <IonItem>
+        <IonInput label="Confirm Password" type="password" value="password"></IonInput>
+      </IonItem>
+
+      
+
+    </IonList>
+
         </IonContent>
       </IonPage>
     );
   };
   
-  export default Register
+  export default Register;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,90 +1,87 @@
 import { 
   IonAvatar,
-    IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonIcon, 
-      IonInput, 
-      IonInputPasswordToggle, 
-      IonItem, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar, 
-      useIonRouter
-  } from '@ionic/react';
+  IonButton,
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonIcon, 
+  IonInput, 
+  IonInputPasswordToggle, 
+  IonItem, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  useIonRouter
+} from '@ionic/react';
 import { logoFacebook, logoInstagram, logoIonic, logoTwitter } from 'ionicons/icons';
-  
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
-  
-    const doLogin = () => {
-        navigation.push('/it35-lab/app','forward','replace');
-    }
-    const Signup = () => {
-      navigation.push('/it35-lab/app','forward','replace');
-  }
-    return (
-      <IonPage>
-        <IonContent className='ion-padding'>
 
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
+
+  const doLogin = () => {
+    navigation.push('/it35-lab/app', 'forward', 'replace'); 
+  };
+
+  const doSignup = () => {
+    navigation.push('/Register', 'forward', 'replace'); 
+  };
+
+  return (
+    <IonPage>
+      <IonContent className='ion-padding'>
         <div style={{
-                  display: 'flex',
-                  flexDirection:'column',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  height: '100%',
-                  width:'100%',
-                  marginTop:'-10rem',
-                  marginBottom:'-18rem',
-                }}>
-             <IonAvatar
-                    style={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      width: '150px',
-                      height: '150px',
-                      borderRadius: '50%', 
-                      overflow: 'hidden' 
-                    }}
-                  >
-        <img alt="" src="https://www.freepik.com/premium-vector/cute-happy-pig-sitting-cartoon-vector-white-background_135404926.htm/" />
-                    {
-                     <IonIcon 
-                      icon={logoInstagram}
-                      color='primary'
-                      style={{ fontSize: '120px', color: '#6c757d' }} 
-                    />
-                    }
-                  </IonAvatar>
-                  <h1 style={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                    }}>USER LOGIN</h1>
-                    
-          </div>
-          
-            <IonTitle>Login</IonTitle>
-          <IonItem>
-        <IonInput label="Email input" type="email" placeholder=""></IonInput>
-      </IonItem>
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+            width: '100%',
+            marginTop: '-10rem',
+            marginBottom: '-18rem',
+          }}>
+          <IonAvatar
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: '150px',
+              height: '150px',
+              borderRadius: '50%',
+              overflow: 'hidden',
+            }}
+          >
+            <img alt="" src="https://www.freepik.com/premium-vector/cute-happy-pig-sitting-cartoon-vector-white-background_135404926.htm/" />
+            <IonIcon 
+              icon={logoInstagram}
+              color='primary'
+              style={{ fontSize: '120px', color: '#6c757d' }} 
+            />
+          </IonAvatar>
+          <h1 style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>USER LOGIN</h1>
+        </div>
+
+        <IonTitle>Login</IonTitle>
+        <IonItem>
+          <IonInput label="Email input" type="email" placeholder=""></IonInput>
+        </IonItem>
 
         <IonInput type="password" label="Password" value="">
-      <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
-    </IonInput>
-            <IonButton onClick={() => doLogin()} expand="full">
-                Login
-                </IonButton>
-                <IonButton onClick={() => Signup()} expand="full">
-                Signup
-            </IonButton>
-        </IonContent>
-      </IonPage>
-      
-    );
-  };
-  
-  export default Login;
+          <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+        </IonInput>
+        
+        <IonButton onClick={() => doLogin()} expand="full">
+          Login
+        </IonButton>
+        <IonButton onClick={() => doSignup()} expand="full">
+          Signup
+        </IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
