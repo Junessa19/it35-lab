8dc963a568ef59bcd8c43e37e0a4c13f7c94bc6d
Junessa19 <bahianjunessa@gmail.com>
2025-04-07 14:19:49 +0800

    Updates

--- assets/md.transition-legacy-CcrDTtXN.js
+++ assets/md.transition-legacy-CcrDTtXN.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-rFhA0qVU.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-NpxQ47x_.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
