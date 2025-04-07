e3064fde5af93f57a2e38d790c06043343c07b2d
Junessa19 <bahianjunessa@gmail.com>
2025-04-07 12:53:36 +0800

    Added supabaseClient.ts

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient('https://dwezgqoxmzopbmjjnzzs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3ZXpncW94bXpvcGJtampuenpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1NzU3NzIsImV4cCI6MjA1OTE1MTc3Mn0.h5TOuE6XQSi-ScM1MbWJ-FtTwN8r_F42JcGqvl7R7Vo');
