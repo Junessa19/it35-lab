73b5cb4f8dad09bccd9f57723c70233095c28982
Junessa19 <bahianjunessa@gmail.com>
2025-04-21 13:59:25 +0800

    Updates

--- assets/md.transition-legacy-CDcp8n_l.js
+++ assets/md.transition-legacy-CDcp8n_l.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-NpxQ47x_.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-B5zYO9FK.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
