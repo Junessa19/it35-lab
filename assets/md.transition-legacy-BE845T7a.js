dfd1fa63e3a9f618b139037adefe3dd63ddb9e96
Junessa19 <bahianjunessa@gmail.com>
2025-04-29 23:47:24 +0800

    Updates

--- assets/md.transition-legacy-BE845T7a.js
+++ assets/md.transition-legacy-BE845T7a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CHk1hs1p.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-nzk75c46.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-Du8r7l9z.js
+++ assets/swipe-back-legacy-Du8r7l9z.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CHk1hs1p.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-nzk75c46.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
